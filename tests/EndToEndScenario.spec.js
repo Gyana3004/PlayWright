894dd79223ff8b0d7079b574ca8f5aa98ec834fa
newtechiee <gyanaprakashsahoo30@gmail.com>
2026-03-29 10:12:56 +0530

    End to End Scenario added

--- tests/EndToEndScenario.spec.js
+++ tests/EndToEndScenario.spec.js
@@ -4,6 +4,7 @@ const {test,expect}=require ('@playwright/test')
 test.only("Windows Handling",async({page})=>
 {
 const productName="ZARA COAT 3";  
+const email="gyanas1997@gmail.com";
 const product=page.locator(".card-body");    
 const Username=page.locator('#userEmail');
 const UserPwd= page.locator("#userPassword");
@@ -34,4 +35,43 @@ await page.locator("div li").first().waitFor();
 const bool=page.locator("h3:has-text('productName')").isVisible();
 expect(bool).toBeTruthy();
 //page.waitForLoadState('networkidle');
+
+await page.locator("text=Checkout").click();
+await page.locator("[placeholder*='Select Country']").pressSequentially("ind", { delay :450 });
+const dropdown=page.locator(".ta-results");
+await dropdown.waitFor();
+const optionscount=await dropdown.locator("button").count();
+for (let i=0;i<optionscount;i++)
+{
+  const text=await dropdown.locator("button").nth(i).textContent();
+  if (text === " India"){
+    await dropdown.locator("button").nth(i).click();
+    break;
+  }
+}
+expect (await page.locator(".user__name [type='text']").first()).toHaveText(email);
+await page.locator(".action__submit ").click();
+await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+const orderId=await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+console.log(orderId)
+//await page.pause();
+await page.locator("button[routerlink*='myorders']").click();
+await page.locator("tbody").waitFor();
+//Validation order id 
+const rows=await page.locator("tbody tr");
+
+
+
+for (let i=0;i<await rows.count();i++){
+
+const rowOrderID=await rows.nth(i).locator("th").textContent();
+if (orderId.includes(rowOrderID)){
+rows.nth(i).locator("button").first().click();
+break;
+}
+
+}
+const orederIDdetails=await page.locator(".col-text ").textContent();
+expect(orderId.includes(orederIDdetails)).toBeTruthy();
+
 });
